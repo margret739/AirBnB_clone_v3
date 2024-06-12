9b2afc27b76427885bfd70b20d720bfb56062fe7
margret739 <margretandalya@gmail.com>
2024-06-12 07:26:40 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Create Flask app"""
+
 import getenv from os
-import Flask from flask
+import Flask, jsonify from flask
 import storage from models
 import app_views from api.v1.views
 
@@ -9,8 +10,20 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """ """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ """
+
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST;, '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(hosts=HOST, port=PORT, threaded=True)
+    app.run(debug=True, hosts=HOST, port=PORT, threaded=True)
