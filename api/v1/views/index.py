29079a6298d067d60f7792ad407ecba0fbf9bff4
margret739 <margretandalya@gmail.com>
2024-06-12 07:27:38 -0700

    upd index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Flask app"""
 
+from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+
 
 @app_views.route('/status')
 def api_status():
@@ -9,3 +12,17 @@ def api_status():
 
     response = {'status': "OK")
     return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+    """ """
+    stats = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'place': storage.count('Place'),
+            'reviews': storage.count('Reviews'),
+            'states': storage.count('States'),
+            'users': storage.count('User')
+            }
+
+    return jsonify(stats)
