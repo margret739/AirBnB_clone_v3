b16461b9ee55b3b07a06219ba677d165d2f0802d
margret739 <margretandalya@gmail.com>
2024-06-14 01:48:43 -0700

    del up

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,6 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST;, '0.0.0.0')
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(debug=True, host=HOST, port=PORT, threaded=True)
