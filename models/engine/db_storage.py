722b2f07bdff73b100e5718bae2e22ca156a35b6
margret739 <margretandalya@gmail.com>
2024-06-14 02:05:58 -0700

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -95,6 +95,6 @@ class DBStorage:
         for clas, value in classes.items():
             if cls == clas or cls == value:
                 all_inst_of_prov_cls = self.all(cls)
-                return len(all_inst_of_prov_cls)
+            return len(all_inst_of_prov_cls)
         if cls not in classes.values():
             return
