e6324bb19062543a669f6e49a7da3a611ce5a6ed
margret739 <margretandalya@gmail.com>
2024-06-14 01:42:09 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Create Flask app"""
 
-import getenv from os
-import Flask, jsonify from flask
-import storage from models
-import app_views from api.v1.views
+from os import getenv
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
